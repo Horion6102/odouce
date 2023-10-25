8f49d5ed82349f5ecc1d2594fd53e856b66c1e06
Valentin LAUNAY <launay.v61@gmail.com>
2023-10-25 19:48:14 +0200

    add global structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,97 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+    <div>
+      <nav>
+        <ul>
+          <li>Addoussisseur d'eau</li>
+          <li>Piscine</li>
+          <li>Contact</li>
+        </ul>
+        <img src="" alt="" />
+      </nav>
+      <div>
+        <h1>Adoucisseur</h1>
+      </div>
+      <main>
+        <article>
+          <div>
+            <h3>O Douce Services</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Viverra sed arcu sed
+              facilisis est tortor a placerat. Est hendrerit enim dui laoreet
+              lorem fermentum cras nibh facilisis. Ac a aliquam non ullamcorper
+              facilisis sagittis. Ut integer sollicitudin amet iaculis volutpat.
+              Lorem ipsum dolor sit amet consectetur. Viverra sed arcu sed
+              facilisis est tortor a placerat.{" "}
+            </p>
+            <button>Contacter</button>
+          </div>
+          <img src="" alt="" />
+        </article>
+        <article>
+          <div>
+            <img src="" alt="" />
+            <h3>O Douce Services</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Viverra sed arcu sed
+              facilisis est tortor a placerat. Est hendrerit enim dui laoreet
+              lorem fermentum cras nibh facilisis. Ac a aliquam non ullamcorper
+              facilisis sagittis. Ut integer sollicitudin amet iaculis volutpat.
+              Lorem ipsum dolor sit amet consectetur. Viverra sed arcu sed
+              facilisis est tortor a placerat.{" "}
+            </p>
+            <button>Contacter</button>
+          </div>
+        </article>
+        <aside>
+          <article>
+            <svg></svg>
+            <h4>Economie d’énergie</h4>
+            <p>Lorem ipsum dolor sit amet consectetur. Elementum viverra augue arcu aliquet amet neque. Eget euismod ultrices varius integer curabitur phasellus ipsum gravida amet. </p>
+          </article>
+          <article>
+            <svg></svg>
+            <h4>Economie d’énergie</h4>
+            <p>Lorem ipsum dolor sit amet consectetur. Elementum viverra augue arcu aliquet amet neque. Eget euismod ultrices varius integer curabitur phasellus ipsum gravida amet. </p>
+          </article>
+          <article>
+            <svg></svg>
+            <h4>Economie d’énergie</h4>
+            <p>Lorem ipsum dolor sit amet consectetur. Elementum viverra augue arcu aliquet amet neque. Eget euismod ultrices varius integer curabitur phasellus ipsum gravida amet. </p>
+          </article>
+        </aside>
+        <h3>Pourquoi s’équiper d’un adoucisseur d’eau ?</h3>
+        <div>accordeon</div>
+      </main>
+      <footer>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h3>Besoin d'information</h3>
+          <p>Contactez moi du lundi au samedi, de 9h à 12h et de 14h à 18H</p>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div>
+          <div>
+            <img src="" alt="" />
+            <a href=""></a>            
+          </div>
+          <div>
+            <img src="" alt="" />
+            <a href=""></a>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+          </div>
+          <div>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 }
