fe9544ef40fa8d8bbd754d1841effcc8885a6573
Valentin LAUNAY <launay.v61@gmail.com>
2023-10-31 23:23:26 +0100

    add css

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,31 @@
+"use client";
 import Image from "next/image";
+import style from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>Addoussisseur d'eau</li>
-        <li>Piscine</li>
-        <li>Contact</li>
+    <nav className={style.globalContainer}>
+      <ul className={style.container}>
+        <li>
+          <a href="" className={style.item}>
+            Addoussisseur d'eau
+          </a>
+        </li>
+        <li>
+          <a href="" className={style.item}>
+            Piscines
+          </a>
+        </li>
+        <li className={style.itemRight}>
+          <a href="" className={style.item}>
+            Contact
+          </a>
+        </li>
       </ul>
-      <Image
+      <img
+        className={style.itemLogo}
         src="/ODouce.png"
         alt="logo de ODouce service"
-        width="64"
-        height="64"
       />
     </nav>
   );
